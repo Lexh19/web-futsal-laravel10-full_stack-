099e784676c3e731b71810c5cc7ae1da1256ae3b
Lexh19 <iqbaalhzd@gmail.com>
2024-01-09 18:00:58 +0700

    tugas uas semster 5

---
+++ resources/views/frontend/pages/register.blade.php
@@ -0,0 +1,143 @@
+@extends('frontend.layouts.master')
+
+@section('title','INF WEB')
+
+@section('main-content')
+	<!-- Breadcrumbs -->
+    <div class="breadcrumbs">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="bread-inner">
+                        <ul class="bread-list">
+                            <li><a href="{{route('home')}}">Home<i class="ti-arrow-right"></i></a></li>
+                            <li class="active"><a href="javascript:void(0);">Register</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Breadcrumbs -->
+
+    <!-- Shop Login -->
+    <section class="shop login section">
+        <div class="container">
+            <div class="row">
+
+                  <!-- /Left Text -->
+                  <div class="col-6">
+                    <div class="container-logo">
+                        <img src="{{ asset('assets/img/backgrounds/Gambarlogin.svg') }}"  class="background-login" >
+                    </div>
+                </div>
+                <!-- /Left Text -->
+
+                  <!-- Login -->
+                <div class="col-lg-6 col-12">
+                    <div class="login-form">
+                            <img src="{{asset  ('assets/img/logo/LogoBola.svg' ) }}" class="logo">
+                        <span class="app-brand-text demo menu-text fw-bold">INF WEB</span>
+                        <h2>Register</h2>
+                        <p>Please register for faster ordering</p>
+                        <!-- Form -->
+                        <form class="form" method="post" action="{{route('register.submit')}}">
+                            @csrf
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label>Your Name<span>*</span></label>
+                                        <input type="text" name="name" placeholder="" required="required" value="{{old('name')}}">
+                                        @error('name')
+                                            <span class="text-danger">{{$message}}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label>Your Email<span>*</span></label>
+                                        <input type="text" name="email" placeholder="" required="required" value="{{old('email')}}">
+                                        @error('email')
+                                            <span class="text-danger">{{$message}}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label>Your Password<span>*</span></label>
+                                        <input type="password" name="password" placeholder="" required="required" value="{{old('password')}}">
+                                        @error('password')
+                                            <span class="text-danger">{{$message}}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label>Confirm Password<span>*</span></label>
+                                        <input type="password" name="password_confirmation" placeholder="" required="required" value="{{old('password_confirmation')}}">
+                                        @error('password_confirmation')
+                                            <span class="text-danger">{{$message}}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group login-btn">
+                                        <button class="btn" type="submit">Register</button>
+                                        <a href="{{route('login.form')}}" class="btn">Login</a>
+                                        OR
+                                        <a href="{{route('login.redirect','facebook')}}" class="btn btn-facebook"><i class="ti-facebook"></i></a>
+                                        <a href="{{route('login.redirect','github')}}" class="btn btn-github"><i class="ti-github"></i></a>
+                                        <a href="{{route('login.redirect','google')}}" class="btn btn-google"><i class="ti-google"></i></a>
+                                    </div>
+                                </div>
+
+                        </form>
+                        <!--/ End Form -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ End Login -->
+@endsection
+
+@push('styles')
+<style>
+    .container-logo{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 100px;
+    }
+    .background-login{
+        width: 500px;
+        height: 300px;
+    }
+    .logo{
+        width: 30px;
+        height: 60px;
+    }
+    .shop.login .form .btn{
+        margin-right:0;
+    }
+    .btn-facebook{
+        background:#39579A;
+    }
+    .btn-facebook:hover{
+        background:#073088 !important;
+    }
+    .btn-github{
+        background:#444444;
+        color:white;
+    }
+    .btn-github:hover{
+        background:black !important;
+    }
+    .btn-google{
+        background:#ea4335;
+        color:white;
+    }
+    .btn-google:hover{
+        background:rgb(243, 26, 26) !important;
+    }
+</style>
+@endpush
